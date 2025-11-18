a4ed1f1d160b2a235fa4cd64e7b6b408ed1bf3a0
rafaelcancelier <145573093+rafaelcancelier@users.noreply.github.com>
2025-11-18 12:03:34 -0300

    1214

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
-
+const botaoJogarNovamente = document.querySelector(".novamente-btn");
 
 
 
