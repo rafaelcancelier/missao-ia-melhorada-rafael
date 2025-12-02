ddd14d3ce1db05b19a481583e28401bb9cc9dba1
rafaelcancelier <145573093+rafaelcancelier@users.noreply.github.com>
2025-12-02 12:03:41 -0300

    124312

--- main.js
+++ main.js
@@ -1,36 +1,41 @@
-import { aleatorio } from "./aleatorio.js";
-import { perguntas } from "./perguntas.js";
+import { aleatorio, nome } from './aleatorio.js';
+import { perguntas } from './perguntas.js';
 
-const caixapPrincipal = document.querySelector(".caixa-principal");
+const caixaPrincipal = document.querySelector(".caixa-principal");
 const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
 const botaoJogarNovamente = document.querySelector(".novamente-btn");
-
-
-
-
-
-
-
-
+const botaoIniciar = document.querySelector(".iniciar-btn");
+const telaInicial = document.querySelector(".tela-inicial");
 
 
 let atual = 0;
 let perguntaAtual;
-let historiaFinal = " ";
+let historiaFinal = "";
 
-function mostraPerguntas() {
+botaoIniciar.addEventListener('click', iniciaJogo);
+
+function iniciaJogo() {
+atual = 0;
+historiaFinal = "";
+telaInicial.style.display = 'none';
+caixaPerguntas.classList.remove("mostrar");
+caixaAlternativas.classList.remove("mostrar");
+caixaResultado.classList.remove("mostrar");
+mostraPergunta();
+}
+
+function mostraPergunta() {
 
     if (atual >= perguntas.length) {
         mostraResultado();
         return;
     }
-
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
-    caixaAlternativas.textContent = " ";
+    caixaAlternativas.textContent = "";
     mostraAlternativas();
 }
 
@@ -38,32 +43,39 @@ function mostraAlternativas() {
     for (const alternativa of perguntaAtual.alternativas) {
         const botaoAlternativas = document.createElement("button");
         botaoAlternativas.textContent = alternativa.texto;
-        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
+        botaoAlternativas.addEventListener('click', () => respostaSelecionada(alternativa));
         caixaAlternativas.appendChild(botaoAlternativas);
     }
 }
-
 
 function respostaSelecionada(opcaoSelecionada) {
     const afirmacoes = aleatorio(opcaoSelecionada.afirmacao);
     historiaFinal += afirmacoes + " ";
     atual++;
-    mostraPerguntas();
+    mostraPergunta();
 }
 
 function mostraResultado() {
-    caixaPerguntas.textContent = "Se fosse possível ...";
+    caixaPerguntas.textContent = `Se fosse possível traduzir sua forma de aprender em palavras, diríamos que ${nome}...`;
     textoResultado.textContent = historiaFinal;
-    caixaAlternativas.textContent = " ";
+    caixaAlternativas.textContent = "";
     caixaResultado.classList.add("mostrar");
     botaoJogarNovamente.addEventListener("click", jogaNovamente);
 }
 
-function jogaNovamente(){
+function jogaNovamente() {
     atual = 0;
-    historiaFinal = " ";
+    historiaFinal = "";
     caixaResultado.classList.remove("mostrar");
-    mostraPerguntas();
+    substituiNome();
+    mostraPergunta();
 }
 
-mostraPerguntas();
+function substituiNome() {
+    for (const pergunta of perguntas) {
+        pergunta.enunciado = pergunta.enunciado.replace(/você/g, nome);
+    }
+}
+
+substituiNome();
+
